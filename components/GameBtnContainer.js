75442972c5aa27353eb4192a246eeb9dd2c5e196
Gonçalo Figueiredo <goncalofigueiredowork@gmail.com>
2024-01-19 15:08:47 +0000

    face drawn btn

--- components/GameBtnContainer.js
+++ components/GameBtnContainer.js
@@ -3,25 +3,29 @@ import useShotlessLoser from '../hooks/useShotlessLoser'
 import CountDownContainer from './CountDownContainer';
 import React from 'react'
 import { connect } from 'react-redux'
+import { useState } from 'react';
 
 const GameBtnContainer = ({goBtnVisible, setGoBtnVisible, players}) => {
 
     const {shotlessLoser, handleDrawName} = useShotlessLoser()
 
+    const [fadeOutDrawBtn, setFadeOutDrawBtn] = useState(false);
+
     const handleClickToStartDraw = () => {
-        console.log("entrei")
-        setGoBtnVisible(false)
+        //setGoBtnVisible(false)
+        setFadeOutDrawBtn(true)
     }
 
     const handleClickOnWinner = () => {
         setGoBtnVisible(true)
+        setFadeOutDrawBtn(false)
     }
     
 
     return (
         <div className="gameBtnContainer">
-            {goBtnVisible ? ( <button className={`drawBtn ${players.length === 0 ? "disabledGoButton" : ""}`} disabled={players.length === 0} onClick={() => {handleClickToStartDraw(); handleDrawName()}}>GO</button> ) : <CountDownContainer shotlessLoser={shotlessLoser} handleClickOnWinner={handleClickOnWinner}/>}
-            {/* <button className="drawBtn" onClick={() => {alert("Done!")}}>GO</button> */}
+            {/* {goBtnVisible ? ( <button className={`drawBtn ${players.length === 0 ? "disabledGoButton" : ""} ${!goBtnVisible ? "fadeOutDrawBtn" : ""}`} disabled={players.length === 0} onClick={() => {handleClickToStartDraw(); handleDrawName()}}>GO</button> ) : <CountDownContainer shotlessLoser={shotlessLoser} handleClickOnWinner={handleClickOnWinner}/>} */}
+            {goBtnVisible ? ( <button onTransitionEnd={()=> {setGoBtnVisible(false)}} className={`drawBtn ${players.length === 0 ? "disabledGoButton" : ""} ${fadeOutDrawBtn ? "fadeOutDrawBtn" : ""}`} disabled={players.length === 0} onClick={() => {handleClickToStartDraw(); handleDrawName()}}>GO</button> ) : <CountDownContainer shotlessLoser={shotlessLoser} handleClickOnWinner={handleClickOnWinner}/>}
         </div>
     )
 }
