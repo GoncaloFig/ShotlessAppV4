dd26d7c846e55c43fa3e9f454eee81741e02ea27
Gonçalo Figueiredo <goncalofigueiredowork@gmail.com>
2024-01-22 01:51:29 +0000

    Revert "try to fix"
    This reverts commit 333644c34249133470b1078c27a39f353a3f6e4f.

--- components/GameContainer.js
+++ components/GameContainer.js
@@ -14,10 +14,20 @@ const GameContainer = () => {
     const [goBtnVisible, setGoBtnVisible] = useState(true);
 
     return (
+        <>
         <div className="gameContainer">
             <h1 className="shotlessTitle">Shotless</h1>
             <GameBtnContainer goBtnVisible={goBtnVisible} setGoBtnVisible={setGoBtnVisible}/>
         </div>
+        {/* SEO PERFORM */}
+        <div className='hidden' style={{display: 'none'}}>
+        <h2 >Who will pay the bill</h2>
+        <h3 >Pick a random person to pay the bill</h3>
+        <h4 >Choose a random person to pay the bill, with Shotless game</h4>
+        <h5 >Draw names to pay the bill</h5>
+        <h6 >This game will generate a random player from a list of player that will be responsible to pay the restaurant or coffee bill</h6>
+        </div>
+        </>
     )
 }
 
