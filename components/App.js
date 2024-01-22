695d31493da12b384109aef9f7fbc818842ab064
Gonçalo Figueiredo <goncalofigueiredowork@gmail.com>
2024-01-22 01:45:15 +0000

    lhast css fix

--- components/App.js
+++ components/App.js
@@ -25,6 +25,16 @@ function App() {
         {visibleEditPlayers ? (<EditPlayersContainer handleOpenEditPlayers={handleOpenEditPlayers}/>) : (<PlayersContainer handleOpenEditPlayers={handleOpenEditPlayers}/>)}
         {/* {visibleEditPlayers ? (<PlayersContainer handleOpenEditPlayers={handleOpenEditPlayers}/>) : null} */}
         </div>
+
+        {/* SEO PERFORM */}
+        <div className='hidden' style={{display: 'none'}}>
+          <h2 className='hidden'>Generate a random person to pay the bill</h2>
+          <h2 >Who will pay the bill</h2>
+          <h3 >Pick a random person to pay the bill</h3>
+          <h4 >Choose a random person to pay the bill, with Shotless game</h4>
+          <h5 >Draw names to pay the bill</h5>
+          <h6 >This game will generate a random player from a list of player that will be responsible to pay the restaurant or coffee bill</h6>
+        </div>
     </Provider>
   );
 }
